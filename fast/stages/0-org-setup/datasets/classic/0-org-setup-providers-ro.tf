4aa2097f33726780bb3e2914488e36497c3b318a
Richard Hart <rhart100@gmail.com>
2026-03-11 11:16:59 +0000

    changed to cicd-rw

--- fast/stages/0-org-setup/datasets/classic/0-org-setup-providers-ro.tf
+++ fast/stages/0-org-setup/datasets/classic/0-org-setup-providers-ro.tf
@@ -21,8 +21,8 @@ terraform {
   }
 }
 provider "google" {
-  impersonate_service_account = "iac-org-ro@rjh01-prod-iac-core-0.iam.gserviceaccount.com"
+  impersonate_service_account = "iac-org-rw@rjh01-prod-iac-core-0.iam.gserviceaccount.com"
 }
 provider "google-beta" {
-  impersonate_service_account = "iac-org-ro@rjh01-prod-iac-core-0.iam.gserviceaccount.com"
+  impersonate_service_account = "iac-org-rw@rjh01-prod-iac-core-0.iam.gserviceaccount.com"
 }
